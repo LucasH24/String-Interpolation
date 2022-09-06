8adbc3feeb9f7eee7c68e773eb0336f6218cd643
Lucas H <lhatfield1@my.wctc.edu>
2022-09-05 19:19:29 -0500

    Add project files.

---
+++ String Interpolation/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace String_Interpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = DateTime.Now;
+            Console.WriteLine($"{date,40:MMMM d, yyyy}");
+            Console.WriteLine($"{date:yyyy.MM.dd}");
+            Console.WriteLine($"{date:'Day' d 'of' MMMM, yyyy}");
+            Console.WriteLine($"{date:'Year:' yyyy, 'Month:' MM, 'Day:' dd}");
+            Console.WriteLine($"{date,10:dddd}");
+            Console.WriteLine($"{date,10:t}{date,10:dddd}");
+            Console.WriteLine($"{date:'h:'h, 'm:'mm, 's:'ss}");
+            Console.WriteLine($"{date:yyyy.MM.dd.h.mm.ss}");
+            Console.WriteLine(date);
+
+            var pi = Math.PI;
+            Console.WriteLine($"{pi:c}");
+            Console.WriteLine($"{pi,10:f3}");
+        }
+    }
+}
